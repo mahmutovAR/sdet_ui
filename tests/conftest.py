a386ee8919b96ea5e016bedc5ea42fe6b7e3b852
mahmutovAR <armax045@gmail.com>
2024-09-21 00:22:55 +0400

    v3 - webdriver_manager for Actions Selenium tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,18 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 pytest_plugins = 'tests.fixtures'
 
 
 @pytest.fixture
 def browser():
-    web_driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    web_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     web_driver.maximize_window()
     yield web_driver
     web_driver.quit()
